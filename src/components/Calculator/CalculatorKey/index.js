28d52d957be05ca6b4e16c474b75eb9b883557ba
Gustavo Reyes <gustavo.reyes@headpspring.com>
2020-06-13 14:46:26 -0500

    Added initial Key component

--- src/components/Calculator/CalculatorKey/index.js
+++ src/components/Calculator/CalculatorKey/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import './style.css';
+import PropTypes from "prop-types";
 
-function CalculatorKey() {
+
+
+const CalculatorKey = (props) => {
+ 
+  const _onClick = () => { // use consts or let
+    props.onClick(props.value)
+  }
+
   return (
     <div className="CalculatorKey">
+      <button onClick={_onClick}>{props.value}</button>
     </div>
   );
 }
+
+CalculatorKey.propTypes = {
+  value: PropTypes.string.isRequired,
+  onClick: PropTypes.func
+}
+
+// Set defaultProps
+CalculatorKey.defaultProps = {
+  value: "X",
+  onClick: null
+};
 
 export default CalculatorKey;
